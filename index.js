72d097da437c45f569728ba999c403dd45bc6e14
r2goyal <r2goyal@gmail.com>
2016-11-25 14:33:01 +0530

    sss

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === verify_token) {
+	if (req.query['hub.verify_token'] === 'inno_chat') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
@@ -53,7 +53,6 @@ app.post('/webhook/', function (req, res) {
 // recommended to inject access tokens as environmental variables, e.g.
 // const token = process.env.PAGE_ACCESS_TOKEN
 const token = "<PAGE_ACCESS_TOKEN>"
-const verify_token = "<VERIFY_TOKEN>"
 
 function sendTextMessage(sender, text) {
 	let messageData = { text:text }
